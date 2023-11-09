9d4ac13b418f81a8f6db59ca06f5c2a1df1bf171
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2023-11-09 02:45:53 +0000

    Update index.tsx

--- components/DealerTransfer/index.tsx
+++ components/DealerTransfer/index.tsx
@@ -4,15 +4,55 @@ import { Dealer } from "../../lib/dealers";
 import RightTable from "./RightTable";
 import LeftTable from "./LeftTable";
 
-type Props = Pick<TransferProps<Dealer>, "dataSource">;
+type Props = Required<Pick<TransferProps<Dealer>, "dataSource">>;
 
 function filterOption(value: string, option: Dealer) {
   return option.search_string.includes(value.toLowerCase());
 }
 
+function randomIndices(arrayLength: number, count = 20) {
+  if (arrayLength < count) {
+    count = arrayLength;
+  }
+
+  const indices = new Set<number>();
+
+  for (let i = 0; i < count; i++) {
+    indices.add(Math.floor(Math.random() * arrayLength));
+  }
+
+  return indices;
+}
+
+function randomTargetKeys(dataSource: Props["dataSource"]) {
+  const indices = randomIndices(dataSource.length);
+
+  return dataSource
+    .filter((d, i) => indices.has(i))
+    .map((d) => d.search_string);
+}
+
 export default function DealerTransfer({ dataSource }: Props) {
   const [ignoredTerms, setIgnoredTerms] = useState(new Set<string>());
-  const [targetKeys, setTargetKeys] = useState<string[]>([]);
+  const [targetKeys, setTargetKeys] = useState<string[]>(
+    randomTargetKeys(dataSource),
+  );
+
+  const handleChange: TransferProps<Dealer>["onChange"] = (
+    nextTargetKeys,
+    _,
+    moveKeys,
+  ) => {
+    for (const key of moveKeys) {
+      ignoredTerms.forEach((value) => {
+        if (key.includes(value.toLowerCase())) {
+          ignoredTerms.delete(value);
+        }
+      });
+    }
+
+    setTargetKeys(nextTargetKeys);
+  };
 
   const handleAddTerm = (term: string) =>
     setIgnoredTerms((prev) => {
@@ -32,7 +72,7 @@ export default function DealerTransfer({ dataSource }: Props) {
       filterOption={filterOption}
       rowKey={(row) => row.search_string}
       showSearch
-      onChange={setTargetKeys}
+      onChange={handleChange}
       targetKeys={targetKeys}
     >
       {({ direction, disabled, filteredItems, selectedKeys, onItemSelect }) =>
