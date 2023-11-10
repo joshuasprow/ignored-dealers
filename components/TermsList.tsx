b4a8c50bd958e7ed6868a2db54002c55dfdf6678
Joshua Sprow <42551798+joshuasprow@users.noreply.github.com>
2023-11-10 00:12:40 +0000

    Update TermsList.tsx

--- components/TermsList.tsx
+++ components/TermsList.tsx
@@ -1,5 +1,6 @@
 import { DeleteOutlined, SearchOutlined } from "@ant-design/icons";
-import { Button, Col, List, Row, Space, Typography } from "antd";
+import { Button, Col, List, Row, Space, Table, Typography } from "antd";
+import { ColumnsType } from "antd/es/table";
 import type { Term, TermKind } from "../lib/terms";
 
 const { Text } = Typography;
@@ -10,7 +11,73 @@ type Props = {
   onRemove: (term: Term) => void;
 };
 
+function TermsTable({ terms, onSearch, onRemove }: Props) {
+  const columns: ColumnsType<Term> = [
+    {
+      dataIndex: "term",
+      title: "Term",
+      defaultSortOrder: "ascend",
+      sorter: (a, b) => a.term.localeCompare(b.term),
+    },
+    {
+      dataIndex: "kind",
+      title: "Kind",
+      render(_, term) {
+        return (
+          <Text style={{ wordBreak: "keep-all" }} type="secondary">
+            {term.kind.replaceAll("dealer_", "")}
+          </Text>
+        );
+      },
+      sorter: (a, b) => a.kind.localeCompare(b.kind),
+    },
+    {
+      key: "search",
+      render(_, term) {
+        return (
+          <Button
+            size="small"
+            type="primary"
+            icon={<SearchOutlined />}
+            onClick={() => {
+              onSearch(term.term);
+            }}
+          />
+        );
+      },
+    },
+    {
+      key: "remove",
+      render(_, term) {
+        return (
+          <Button
+            size="small"
+            danger
+            icon={<DeleteOutlined />}
+            onClick={() => {
+              onRemove(term);
+            }}
+          />
+        );
+      },
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={Array.from(terms.entries()).map<Term>(([term, kind]) => ({
+        kind,
+        term,
+      }))}
+      columns={columns}
+      size="small"
+      pagination={false}
+    />
+  );
+}
 export default function TermsList({ terms, onSearch, onRemove }: Props) {
+  if (true)
+    return <TermsTable terms={terms} onSearch={onSearch} onRemove={onRemove} />;
   return (
     <List>
       {Array.from(terms.entries()).map(([term, kind]) => (
