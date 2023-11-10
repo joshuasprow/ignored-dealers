7cd6cd598fe344b617c7ed447fc523a8069c7cdf
Joshua Sprow <jsprow@gmail.com>
2023-11-10 13:34:41 -0500

    using tanstack query for fetching

--- hooks/use-dealers.ts
+++ hooks/use-dealers.ts
@@ -1,20 +1,18 @@
 import uFuzzy from "@leeoniya/ufuzzy";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Dealer } from "../lib/dealers";
+import { useQuery } from "@tanstack/react-query";
 
 const uf = new uFuzzy({ intraIns: 1, intraChars: `[a-z\d'\-\.]` });
 
-let dealers: Dealer[] = [];
-let queries: string[] = [];
-
-function filter(search: string) {
+function filter(dealers: Dealer[], queries: string[], search: string) {
   if (!search) {
     return dealers;
   }
 
   if (dealers.length !== queries.length) {
     throw new Error(
-      `dealers ${dealers.length} and queries ${queries.length} out of sync`,
+      `dealers ${dealers.length} and queries ${queries.length} out of sync`
     );
   }
 
@@ -24,7 +22,7 @@ function filter(search: string) {
     return [];
   }
 
-  const next: (typeof dealers)[number][] = [];
+  const next: Dealer[] = [];
 
   for (const index of indices) {
     next.push(dealers[index]);
@@ -33,42 +31,43 @@ function filter(search: string) {
   return next;
 }
 
-export default function useDealers() {
-  const [filtered, setFiltered] = useState<Dealer[]>([]);
-  const [search, setSearch] = useState("");
-
-  const init = async (signal: AbortSignal) => {
-    const res = await fetch("api/dealers", { signal });
+async function queryFn() {
+  try {
+    const res = await fetch("api/dealers");
     const json = await res.json();
 
-    dealers = json;
-    queries = dealers.map((d) => d.query);
+    return json as Dealer[];
+  } catch (error) {
+    console.error(error);
 
-    setFiltered(dealers);
-  };
+    return [];
+  }
+}
+
+export default function useDealers() {
+  const { data: dealers } = useQuery<Dealer[]>({
+    initialData: [],
+    queryFn,
+    queryKey: ["dealers"],
+  });
+
+  const [filtered, setFiltered] = useState(dealers);
+  const [search, setSearch] = useState("");
+
+  const queries = useMemo(() => dealers.map((d) => d.query), [dealers]);
 
   useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
+    console.log("rerendering");
+    setFiltered(() => {
+      if (!search) return dealers;
 
-    init(signal);
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
+      return filter(dealers, queries, search);
+    });
+  }, [dealers, queries, search]);
 
   return {
     dealers: filtered,
     search,
-    onSearch: (s: string) => {
-      setSearch(s);
-
-      setFiltered(() => {
-        if (!s) return dealers;
-
-        return filter(s);
-      });
-    },
+    onSearch: setSearch,
   };
 }
